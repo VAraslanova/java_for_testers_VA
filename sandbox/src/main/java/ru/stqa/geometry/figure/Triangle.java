e60f3470ebef9580dc11d8935e9a985cb48e50b0
VAraslanova <araslanovaleriya@gmail.com>
2024-08-17 08:50:57 +0300

    Задание 3. Использование ветвления и исключения

--- sandbox/src/main/java/ru/stqa/geometry/figure/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figure/Triangle.java
@@ -10,6 +10,14 @@ public class Triangle {
         this.s1 = s1;
         this.s2 = s2;
         this.s3 = s3;
+
+        if (s1 < 0 || s2 < 0 || s3 < 0){
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+
+        if (s1 + s2 < s3 || s1 + s3 < s2 || s2 + s3 < s1) {
+            throw new IllegalArgumentException("Side of triangle too long");
+        }
     }
 
     public static void printPerimeterResult(Triangle triangle){
--- sandbox/src/test/java/ru/stqa/qeometry/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/qeometry/TriangleTests.java
@@ -7,16 +7,36 @@ import ru.stqa.geometry.figure.Triangle;
 public class TriangleTests {
 
     @Test
-    void CheckPerimeter() {
+    void checkPerimeter() {
         Triangle triangle = new Triangle(8, 5, 5);
         double result = triangle.perimeter();
         Assertions.assertEquals(18, result);
     }
 
     @Test
-    void CheckArea() {
+    void checkArea() {
         Triangle triangle = new Triangle(8, 5, 5);
         double result = triangle.area();
         Assertions.assertEquals(12, result);
+    }
+
+    @Test
+    void checkExistenceNegativeSide() {
+        try {
+            new Triangle(-5.0, -1.0, -3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
+
+    @Test
+    void checkSumOfSides(){
+        try {
+            new Triangle(10.0, 1.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
     }
 }
