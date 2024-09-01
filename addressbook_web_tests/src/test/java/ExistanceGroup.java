ccf79da8887ae17b88495abdc1a10b1fea1892cf
VAraslanova <araslanovaleriya@gmail.com>
2024-09-01 14:45:54 +0300

    Добавление проверки на существование группы

--- addressbook_web_tests/src/test/java/ExistanceGroup.java
+++ addressbook_web_tests/src/test/java/ExistanceGroup.java
@@ -20,7 +20,8 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class GroupDeleteTests {
+
+public class ExistanceGroup {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
@@ -29,14 +30,13 @@ public class GroupDeleteTests {
     driver = new ChromeDriver();
     js = (JavascriptExecutor) driver;
     vars = new HashMap<String, Object>();
-
   }
   @AfterEach
   public void tearDown() {
     driver.quit();
   }
   @Test
-  public void test2() {
+  public void test3() {
     driver.get("http://localhost/addressbook/");
     driver.manage().window().setSize(new Dimension(1905, 1022));
     driver.findElement(By.name("user")).sendKeys("admin");
@@ -45,17 +45,6 @@ public class GroupDeleteTests {
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("selected[]")).click();
     driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.linkText("Logout")).click();
-
-    driver.get("http://localhost/addressbook/");
-    driver.manage().window().setSize(new Dimension(1905, 1022));
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.name("pass")).sendKeys(Keys.ENTER);
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("selected[]")).click();
-    driver.findElement(By.name("delete")).click();
-    driver.findElement(By.linkText("group page")).click();
     driver.findElement(By.linkText("Logout")).click();
   }
 }
