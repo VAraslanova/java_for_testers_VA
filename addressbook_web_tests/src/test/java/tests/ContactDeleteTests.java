cf75a7cb0a7e1a74d29c3e3bf14afceb11fd5ef1
VAraslanova <araslanovaleriya@gmail.com>
2024-12-01 16:44:19 +0300

    лекция 4. сравнение контактов при удалении

--- addressbook_web_tests/src/test/java/tests/ContactDeleteTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactDeleteTests.java
@@ -1,12 +1,13 @@
 package tests;
 
 import model.ContactData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ContactDeleteTests extends TestBase {
 
-    @Test
-    public void canDeleteContact() {
+    @Test //в приложении ошибка: контакты не удаляются, проверяем, что список не меняется
+    public void canNotDeleteContact() {
         if (app.contacts().isContactPresent()) {
             app.contacts().createContact(new ContactData("", "First name", "Middle name", "Last name", //"Nickname", "Photo",
                     //"Title", "Company", "Address",
@@ -14,6 +15,9 @@ public class ContactDeleteTests extends TestBase {
                     //"Email", "Email2", "Email3", "Homepage", "Birthday", "Anniversary", "Group"
                     ));
         }
+        var oldGroups = app.contacts().getList();
         app.contacts().removeContact();
+        var newGroups = app.contacts().getList();
+        Assertions.assertEquals(oldGroups, newGroups);
     }
 }
