047e2831f200643c4e17e3e572662288a4cd6f1e
VAraslanova <araslanovaleriya@gmail.com>
2025-01-09 15:13:29 +0300

    Настройка проекта mantis

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -9,17 +9,17 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class TestBase {
-    protected static ApplicationManager app;
+        protected static ApplicationManager app;
 
-    @BeforeAll
-    public static void setUp() throws IOException {
-        if (app == null) {
-            var properties = new Properties();
-            properties.load(new FileReader(System.getProperty("target", "local.properties")));
-            app = new ApplicationManager();
-            app.init(System.getProperty("browser", "chrome"), properties);
+        @BeforeAll
+        public static void setUp() throws IOException {
+            if (app == null) {
+                var properties = new Properties();
+                properties.load(new FileReader(System.getProperty("target", "local.properties")));
+                app = new ApplicationManager();
+                app.init(System.getProperty("browser", "chrome"), properties);
+            }
         }
-    }
 
     @AfterEach
     void checkDatabaseConsistency() {
